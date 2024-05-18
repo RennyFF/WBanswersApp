37970db81048633bbe836930c02a5a125b1a572f
root <steam_for_akk@mail.ru>
2024-05-19 01:12:47 +0300

    some improvements and new functionality

--- WBNEWANSWEARS/MVVM/ViewModel/SettingsViewModel.cs
+++ WBNEWANSWEARS/MVVM/ViewModel/SettingsViewModel.cs
@@ -5,11 +5,30 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WBNEWANSWEARS.MVVM.ViewModel
 {
     class SettingsViewModel : Core.ViewModel
     {
+        private ObservableCollection<int> priorityNumbers = new ObservableCollection<int>()
+        {
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+        };
+        public ObservableCollection<int> PriorityNumbers
+        {
+            set => priorityNumbers = value;
+            get => priorityNumbers;
+        }
         private ObservableCollection<UsersStructure> _users;
         public ObservableCollection<UsersStructure> Users
         {
@@ -23,7 +42,7 @@ namespace WBNEWANSWEARS.MVVM.ViewModel
             set
             {
                 _selectedUser = value;
-                onPropertyChanged("SelectedUser");
+                onPropertyChanged(nameof(SelectedUser));
             }
             get => _selectedUser;
         }
